093a9f3338b3e730a7ccb5f2c3c0ed0064887a49
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2016-02-24 11:27:54 +0000

    random addition problem creatio (step 2)

--- vscsharptut2/Maths Quiz/Maths Quiz/Form1.cs
+++ vscsharptut2/Maths Quiz/Maths Quiz/Form1.cs
@@ -12,9 +12,45 @@ namespace Maths_Quiz
 {
     public partial class Form1 : Form
     {
+        Random randomizer = new Random();
+
+        //integer variables to store the numbers for teh addition problem
+        int addend1;
+        int addend2;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Start the quiz by filling in all of the problems
+        /// and starting the timer
+        /// </summary>
+        public void StartTheQuiz()
+        {
+            //fill in the addition problem.
+            //Generate 2 random numbers to add
+            //store the values in the variables 'addend1' and 'addend2'
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            //convert the two randomly generated numbers
+            //into strings so that they can be displayed 
+            //in the label controls
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            //'sum' is the name of the NumericUpDown control.
+            //Thus step makes sure its value is zero before 
+            //adding any values to it
+            sum.Value = 0;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
     }
 }
