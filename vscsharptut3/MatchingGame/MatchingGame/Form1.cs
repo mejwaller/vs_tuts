88ac712973a13901da2dad9624a0af1f01e026a7
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2016-03-15 14:14:55 +0000

    Add (and hide) random icons to board (step 3)

--- vscsharptut3/MatchingGame/MatchingGame/Form1.cs
+++ vscsharptut3/MatchingGame/MatchingGame/Form1.cs
@@ -28,11 +28,35 @@ namespace MatchingGame
         public Form1()
         {
             InitializeComponent();
+
+            AssignIconsToSquares();
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
+
+        /// <summary> 
+        /// Assign each icon from the list of icons to a random square 
+        /// </summary> 
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels, 
+            // and the icon list has 16 icons, 
+            // so an icon is pulled at random from the list 
+            // and added to each label 
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        } 
     }
 }
