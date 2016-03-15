904502ba1b639b36c89e4c236e20ee441a7c618e
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2016-03-15 16:23:58 +0000

    Add timer to hide icons after 2 selected (step 6)

--- vscsharptut3/MatchingGame/MatchingGame/Form1.cs
+++ vscsharptut3/MatchingGame/MatchingGame/Form1.cs
@@ -75,6 +75,13 @@ namespace MatchingGame
         /// <param name="e"></param>
         private void label_Click(object sender, EventArgs e)
         {
+
+            // The timer is only on after two non-matching  
+            // icons have been shown to the player,  
+            // so ignore any clicks if the timer is running 
+            if (timer1.Enabled == true)
+                return;
+
             Label clickedLabel = sender as Label;
 
             if (clickedLabel != null)
@@ -93,10 +100,46 @@ namespace MatchingGame
                 {
                     firstClicked = clickedLabel;
                     firstClicked.ForeColor = Color.Black;
-
                     return;
                 }
+
+                // If the player gets this far, the timer isn't 
+                // running and firstClicked isn't null, 
+                // so this must be the second icon the player clicked 
+                // Set its color to black
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                // If the player gets this far, the player  
+                // clicked two different icons, so start the  
+                // timer (which will wait three quarters of  
+                // a second, and then hide the icons)
+                timer1.Start();
             }
+        }
+
+        /// <summary> 
+        /// This timer is started when the player clicks  
+        /// two icons that don't match, 
+        /// so it counts three quarters of a second  
+        /// and then turns itself off and hides both icons 
+        /// </summary> 
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the timer
+            timer1.Stop();
+
+            // Hide both icons
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            // Reset firstClicked and secondClicked  
+            // so the next time a label is 
+            // clicked, the program knows it's the first click
+            firstClicked = null;
+            secondClicked = null;
         } 
     }
 }
