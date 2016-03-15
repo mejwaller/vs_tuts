10cd7f46b8aad99e51d56148e8cb8fa293dd430c
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2016-03-15 14:06:30 +0000

    Addrandomizer and list of icons (step 2)

--- vscsharptut3/MatchingGame/MatchingGame/Form1.cs
+++ vscsharptut3/MatchingGame/MatchingGame/Form1.cs
@@ -12,6 +12,19 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+
+        // Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon 
+        // in the Webdings font, 
+        // and each icon appears twice in this list
+        List<string> icons = new List<string>() 
+        { 
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        }; 
+        
         public Form1()
         {
             InitializeComponent();
